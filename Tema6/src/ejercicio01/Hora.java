9c4eb4e15cb82c5f3b94b37bf9e8d41f3346d70e
jpgonzalez <jpgonzalez@106-03.info2>
2024-04-02 14:15:56 +0200

    Commit

---
+++ Tema6/src/ejercicio01/Hora.java
@@ -0,0 +1,65 @@
+package ejercicio01;
+
+public class Hora {
+
+	protected int hora;
+	protected int minuto;
+
+	public Hora(int hora, int minuto) {
+		if (hora >= 0 && hora < 24) {
+			this.hora = hora;
+		}
+		if (minuto > 0 && minuto < 60) {
+			this.minuto = minuto;
+		}
+
+	}
+
+	public boolean setMinutos(int valor) {
+		boolean res = false;
+
+		if (valor >= 0 && valor < 60) {
+			this.minuto = valor;
+			res = true;
+		}
+
+		return res;
+	}
+
+	public boolean setHoras(int valor) {
+		boolean res = false;
+
+		if (valor >= 0 && valor < 24) {
+			this.hora = valor;
+			res = true;
+		}
+
+		return res;
+	}
+
+	@Override
+	public String toString() {
+		String res = "";
+
+		if (hora < 10) {
+			res += "0" + this.hora + ":" + this.minuto;
+			if (minuto < 10) {
+				res += this.hora + ":0" + this.minuto;
+			} else {
+				res += this.hora + ":" + this.minuto;
+			}
+
+		} else {
+			res += this.hora + ":" + this.minuto;
+			if (minuto < 10) {
+				res += this.hora + ":0" + this.minuto;
+			} else {
+				res += this.hora + ":" + this.minuto;
+			}
+
+		}
+
+		return res;
+	}
+
+}
